b3fcd75a955e6d0ee00e3431b41e455c19329c35
bradlycarpenter <bradlycarpenterza@gmail.com>
2025-01-04 17:27:38 +0200

    update/facts

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,36 @@
-#include "grid.h"
 #include "raylib.h"
-#include "square.h"
+#include <stdio.h>
 #include <vector>
+
+typedef struct {
+  float Lifetime;
+} Timer;
+
+void StartTimer(Timer *timer, float lifetime) {
+  if (timer != NULL)
+    timer->Lifetime = lifetime;
+}
+
+void UpdateTimer(Timer *timer) {
+  if (timer != NULL && timer->Lifetime > 0)
+    timer->Lifetime -= GetFrameTime();
+}
+
+bool TimerDone(Timer *timer) {
+  if (timer != NULL)
+    return timer->Lifetime <= 0;
+
+  return false;
+}
 
 int main() {
 
   SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
 
   InitWindow(640, 480, "Snake");
-  SetTargetFPS(10);
+  SetTargetFPS(120);
+
+  Timer move_timer{};
 
   std::vector<std::vector<int>> grid = {{}, {}};
   for (std::size_t i = 0; i < 680; i += 40) {
@@ -19,27 +41,39 @@ int main() {
     grid[1].push_back(i);
   }
 
-  Square square;
-  square.grid = grid;
-
-  Grid draw_grid;
-	draw_grid.grid = grid;
-
   while (!WindowShouldClose()) {
+    if (IsKeyDown(KEY_W)) {
+    }
+    if (IsKeyDown(KEY_S)) {
+    }
+    if (IsKeyDown(KEY_A)) {
+    }
+    if (IsKeyDown(KEY_D)) {
+    }
+
     ClearBackground(BLACK);
-
-    if (IsKeyDown(KEY_D))
-      square.posX += 1;
-    if (IsKeyDown(KEY_A))
-      square.posX -= 1;
-    if (IsKeyDown(KEY_W))
-      square.posY += -1;
-    if (IsKeyDown(KEY_S))
-      square.posY += 1;
-
     BeginDrawing();
-    square.update();
-		draw_grid.update();
+
+    UpdateTimer(&move_timer);
+    if (TimerDone(&move_timer)) {
+      StartTimer(&move_timer, 0.5f);
+      printf("Tick");
+    }
+
+    for (std::size_t i{0}; i < grid[0].size() - 1; i++) {
+      std::size_t posX = grid[0][i];
+      std::size_t posY_start = grid[1][0];
+      std::size_t posY_end = grid[1][grid[1].size() - 1];
+      DrawLine(posX, posY_start, posX, posY_end, WHITE);
+    }
+
+    for (std::size_t j{0}; j < grid[1].size() - 1; j++) {
+      std::size_t posY = grid[1][j];
+      std::size_t posX_start = grid[0][0];
+      std::size_t posX_end = grid[0][grid[0].size() - 1];
+      DrawLine(posX_start, posY, posX_end, posY, WHITE);
+    }
+
     EndDrawing();
   }
 
